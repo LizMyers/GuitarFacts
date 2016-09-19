85969e989eb8137c81e57ac57404e0b7fd6265c4
Myers Design Ltd <lizmyers@amazon.com>
2016-09-18 17:44:35 -0700

    Initial Commit
    here we go

---
+++ src/content/en-GB/gb-facts.js
@@ -0,0 +1,19 @@
+var FACTS = [
+    "The strings of guitars are often called gut strings because originally the strings were made from sheep and goat guts.",
+    "A seven-stringed guitar is called a gypsy guitar.",
+    "The world\’s smallest guitar is 10 micrometers long with strings 50 nanometers (100 atoms) wide.",
+    "The oldest guitar-like instrument is around 3500 years old.",
+    "Guitars were first played in Spain in the 14th century where they developed out of an instrument called a vihuela. The first music for the guitar was published in 1546.",
+    "The largest guitar ensemble was achieved by 6,346 participants in Poland on 1st May 2009.",
+    "The word Guitar comes from different languages from the Spanish guitarra, to the Arabic qitara all the way back to ancient Greece\’s kithara.",
+    "Strings used to be made from animal intestines prior to modern nylon technology.",
+    "Many classical guitarists borrow music written for other instruments for performance since the guitar has not been around a long as some others.",
+    "The name guitar has its origins in the Latin word Cithara. It wasn\'t until the 1200s that an instrument began to use the name. The Guitarra Moresca (Moorish Guitar) and Guitarra Latina (Latin Guitar). ",
+    "Andrés Segovia, was a virtuoso Spanish classical guitarist from Linares, Spain. He is regarded as one of the greatest guitarists of all time and the grandfather of the classical guitar.",
+    "Segovia\'s first public performance was in Granada at the age of 16 in 1909. A few years later he played his first professional concert in Madrid, which included works by Francisco Tárrega and his own guitar transcriptions of J.S. Bach.",
+    "Julian Bream, an English classical guitarist and lutenist, played a significant role in altering the public perception of the classical guitar as a respectable instrument.",
+    "Fernando Sor was one of the most prolific composers for, and promoters of, the guitar as a concert instrument, in the last two hundred years. He paved the way for Andrés Segovia and others.",
+    "Heiter Villa-Lobos is one of the most important composers for the classical guitar. His Choros Number 1 was made famous by the film, The Deer Runner."
+];
+
+module.exports = FACTS;
---
+++ src/index.js
@@ -0,0 +1,99 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var SKILL_NAME = 'Classical Guitar Facts';
+var FACTS = [];
+var location = " ";
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+ //var LOCATION = this.event.request.locale;
+       // console.log("location: "+LOCATION);
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        /*----CHECK REQUEST LOCATION----*/
+        location = this.event.request.locale;
+        console.log("location: "+ location);
+        //location = "de-DE";
+
+        /*----USE LOCALIZED CONTENT----*/
+        switch(location) {
+            case 'en-US':
+            FACTS = require('./content/en-US/us-facts.js');
+            console.log("Getting FACTS in US English");
+            break;
+            case 'en-GB':
+                try {
+                    FACTS = require('./content/en-GB/gb-facts.js');
+                    console.log("Getting FACTS in GB English");
+                } catch (err) {
+                    FACTS = require('./content/en-US/us-facts.js');
+                    console.log("Getting FACTS in US English");
+                }
+            break;
+            case 'de-DE':
+                try {
+                    FACTS = require('./content/de-DE/de-facts.js');
+                    console.log("Getting FACTS auf Deutsch");
+                } catch (err) {
+                    FACTS = require('./content/en-US/us-facts.js');
+                    console.log("Getting FACTS in US English");
+                }
+            break;
+            case 'default':
+            FACTS = require('./content/en-US/us-facts.js');
+            console.log("Getting FACTS in US English");
+            break;
+        }
+        /*---------------------------------------*/
+        this.emit('GetFact' , FACTS);
+    },
+    'GetFact': function () {
+        // Get a random space fact from the space facts list
+        var factIndex = Math.floor(Math.random() * FACTS.length);
+        var randomFact = FACTS[factIndex];
+
+        // Create speech output
+        if (location == 'de-DE') {
+            var speechOutput = "Hier ist Deine Tatsache: " + randomFact;
+        } else {
+            var speechOutput = "Here's your fact: " + randomFact;
+        }
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function (location) {
+        if (location === 'de-DE') {
+            var speechOutput = "Du kannst sagen, sag mir eine Gitarretatsache, oder Du kannst Stop sagen.  Wie kann ich Dir helfen?";
+            var reprompt = "Du kannst sagen, sag mir eine Gitarretatsache, oder Du kannst Stop sagen.  Wie kann ich Dir helfen?";
+        } else {
+            var speechOutput = "You can say tell me a guitar fact, or, you can say stop... How can I help?";
+            var reprompt = "You can say tell me a guitar fact, or, you can say stop";
+        }
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function (location) {
+        if (location === 'de-DE') {
+            this.emit(':tell', 'Auf Wiedersehen!');
+        } else {
+             this.emit(':tell', 'Goodbye!');
+        }
+    },
+    'AMAZON.StopIntent': function (location) {
+        if (location === 'de-DE') {
+            this.emit(':tell', 'Auf Wiedersehen!');
+        } else {
+             this.emit(':tell', 'Goodbye!');
+        }
+    }
+};
