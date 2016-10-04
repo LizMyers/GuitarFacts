429495634f5e9800757717e84f1f5542d4cd6a03
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2016-10-04 22:36:29 +0200

    Find some typos

--- src/content/de-DE/de-facts.js
+++ src/content/de-DE/de-facts.js
@@ -1,19 +1,19 @@
 var FACTS = [
-    "Gitarrensaiten werden oft Darmsaiten genannt, weil sie ursprünglich aus Schafs- und Ziegendärme gemacht worden sind.",
+    "Gitarrensaiten werden oft Darmsaiten genannt, weil sie ursprünglich aus Schafs- und Ziegendärmen hergestellt worden sind.",
     "Eine Gitarre mit sieben Saiten heißt Gypsy-Gitarre, früher auch Zigeunergitarre genannt.",
-    "Das kleinste Gitarre der Welt ist 10 Mikrometer lange mit 50 Nanometer, das sind 100 Atome, breiten Saiten.",
-    "Das älteste Gitarren ähnliche Instrument ist ungefaehr 3500 Jahre alt.",
+    "Das kleinste Gitarre der Welt ist 10 Mikrometer lang mit 50 Nanometer, das sind 100 Atome, breiten Saiten.",
+    "Das älteste Gitarren-ähnliche Instrument ist ungefähr 3500 Jahre alt.",
     "Das erste Mal wurden Gitarren im vierzehnten Jahrhundert in Spanien gespielt. Sie stammt von der Vihuela ab. Die erste Musik für die Gitarre wurde 1546 veröffentlicht.",
     "Das größte Gitarrenensemble gab es am ersten Mai 2009 in Polen mit 6.346 Teilnehmern.",
-    "Das Wort, Gitarre, stammt aus verschiedene Sprachen ab– von der spanischen Gitarre, dem arabischen Qitara, bis zum uralten griechischen Kithara.",
-    "BVor der modernen Nylontechnologie wurden Saiten aus Tierdärmen gemacht.",
-    "Viele klassische Gitarrenspieler nutzen Musik, die für andere Insturmente geschrieben wurde, da es die Gitarre noch nicht so lange gibt.",
-    "Das Wort, Gitarre, stammt aus dem Lateinischen, Cithara. Erst ab dem 13.Jahrhundert wurde das Instrument Gitarre genannt. (Der Name stammt aus dem moorischen und lateinischen ab.)",
-    "Andrés Segovia, der aus Linares in Spanien stammte, war ein virtuoser klassicher Gitarrenspieler. Er wird als Meister und Grossvater der klassischen Gitarre gesehen.",
-    "Um 1909 gab Segovia seinen ersten öffentlichen Vortrag in Grenada. Er war 16 Jahre alt. Ein paar Jahre später, gab er sein erstes professionelles Konzert in Madrid. Er spielte sowohl die Musik von Francisco Tarrèga, als auch seine eigenen Transkription von Bach.",
-    "Julien Bream, ein klassicher Gitarren- und Lautenspieler aus England, spielte eine wichtige Rolle, die klassichen Gitarre als seriöses Instrument zu etablieren.",
-    "Fernando Sor war einer der produktivsten Konponisten und Promoter der Guitarre als Konzertinstrument in den letzten zweihundert Jahren. Er hat den Weg für Andrés Segovia und Andere geebnet.",
-    "Heiter Villa-Lobos ist einer der wichtigsten klassische Gitarrekomponiste. Mit dem Film, Deer Runner, ist sein Choros Nummer 1 weltberühmt geworden."
+    "Das Wort, Gitarre, stammt von verschiedenen Sprachen ab – von der spanischen Gitarre, über die arabische Qitara, bis zur uralten griechischen Kithara.",
+    "Vor der modernen Nylontechnologie wurden Saiten aus Tierdärmen hergestellt.",
+    "Viele klassische Gitarrenspieler nutzen Musik, die für andere Instrumente geschrieben wurde, da es die Gitarre noch nicht so lange gibt.",
+    "Das Wort, Gitarre, stammt aus dem Lateinischen, Cithara. Erst ab dem 13. Jahrhundert wurde das Instrument Gitarre genannt. (Der Name stammt vom moorischen und lateinischen ab.)",
+    "Andrés Segovia, der aus Linares in Spanien stammte, war ein virtuoser klassicher Gitarrenspieler. Er wird als Meister und Großvater der klassischen Gitarre gesehen.",
+    "Um 1909 gab Segovia seinen ersten öffentlichen Vortrag in Grenada. Er war 16 Jahre alt. Ein paar Jahre später gab er sein erstes professionelles Konzert in Madrid. Er spielte sowohl die Musik von Francisco Tarrèga, als auch seine eigenen Transkription von Bach.",
+    "Julien Bream, ein klassicher Gitarren- und Lautenspieler aus England, spielte eine wichtige Rolle, die klassische Gitarre als seriöses Instrument zu etablieren.",
+    "Fernando Sor war einer der produktivsten Komponisten und Promoter der Gitarre als Konzertinstrument in den letzten zweihundert Jahren. Er hat den Weg für Andrés Segovia und Andere geebnet.",
+    "Heiter Villa-Lobos ist einer der wichtigsten klassischen Gitarrekomponisten. Mit dem Film Deer Runner ist sein Choros Nummer 1 weltberühmt geworden."
 ];
 
 module.exports = FACTS;
